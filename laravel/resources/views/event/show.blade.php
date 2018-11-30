e6f980cb20cfbcb0d6d498d629963b6be9bcdbed
Marcos Evaldt <marcosevaldt@gmail.com>
2018-11-30 02:32:35 -0200

    Alterado para post method da rota em direcao ao checkout

--- laravel/resources/views/event/show.blade.php
+++ laravel/resources/views/event/show.blade.php
@@ -7,18 +7,19 @@
             <div class="card">
                 <div class="card-header">{{ $event->name }}</div>
                 <div class="card-body">
-                    <form method="POST" action="{{ route('checkout.index', ['id' => $event->id]) }}">
-                        <div class="form-group">
-                            <h5>Nome do Evento</h5>
-                            <p>{{ $event->name }}</p>
-                            <h5>Local de Apresentação</h5>
-                            <p>{{ $event->address }}</p>
-                            <h5>Data</h5>
-                            <p>{{ $event->date->format('d/m/Y') }}</p>
-                            <h5>Horário</h5>
-                            <p>{{ $event->date->format('H:i:s') }}</p>
-                        </div>
-                        <input type="submit" class="btn btn-sm btn-primary" value="Comprar">
+                    <div class="form-group">
+                        <h5>Nome do Evento</h5>
+                        <p>{{ $event->name }}</p>
+                        <h5>Local de Apresentação</h5>
+                        <p>{{ $event->address }}</p>
+                        <h5>Data</h5>
+                        <p>{{ $event->date->format('d/m/Y') }}</p>
+                        <h5>Horário</h5>
+                        <p>{{ $event->date->format('H:i:s') }}</p>
+                    </div>
+                    <form method="POST" action="{{ route('checkout.index') }}">
+                        <input type="hidden" name="id" value="{{ $event->id }}">
+                        <input type="submit" value="Comprar" class="btn btn-sm btn-primary">
                         @csrf
                     </form>
                 </div>
